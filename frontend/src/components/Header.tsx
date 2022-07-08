58d0dad5850db1398e41c1f67745d2127dc65e98
Vinicius Perrone <perronevinicius2018@gmail.com>
2022-07-08 20:23:14 -0300

    style: stylized home screen

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,11 +1,11 @@
 import Image from 'next/image';
-import { List } from 'phosphor-react';
+import { FiMenu } from 'react-icons/fi';
 import LogoSvg from '../assets/icons/logo.svg';
 
 export function Header() {
   return (
     <header className="bg-blue-400 h-10 px-5 flex items-center justify-between">
-      <List size={40} weight="light" className="text-white" />
+      <FiMenu size={36} className="text-white" />
       <Image src={LogoSvg} />
       <div></div>
     </header>
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -12,25 +12,25 @@ export default function Home() {
       <main className="flex-1 pt-[30px]">
         <div className="flex w-full pl-[18.2vw] items-center gap-10">
           <span>
-            <Image src={PlusSvg} width={65} height={65} />
+            <Image src={PlusSvg} width={60} height={60} />
           </span>
-          <Image src={FillingSvg} width={115} height={115} />
-          <strong className="text-[56px] text-blue-400 font-flexoBold leading-[56px]">
+          <Image src={FillingSvg} width={100} height={100} />
+          <strong className="text-[48px] text-blue-400 font-flexoBold leading-[48px]">
             Simulação
             <br /> de Taxas
           </strong>
         </div>
         <div className="flex-1 flex flex-col items-center pt-[68px]">
-          <h2 className="text-[40px] text-blue-400 font-flexoBold">
+          <h2 className="text-[36px] text-blue-400 font-flexoBold">
             Valor desejado
           </h2>
           <div className="flex flex-row gap-[30px] py-7">
             <input
               type="number"
               placeholder="R$ 0,00"
-              className="bg-gray-200 w-[368px] h-10 text-center"
+              className="bg-gray-200 w-[368px] h-[60px] px-2 text-center text-[24px] font-flexoRegular leading-10"
             />
-            <button className="bg-orange-400 text-gray-200 w-[160px]">
+            <button className="bg-orange-400 w-[160px] rounded-[5px] py-2 text-[20px] font-flexoBold text-gray-200 leading-[24px] ">
               Calcular
             </button>
           </div>
