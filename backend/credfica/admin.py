0c67c59a23c4c77f7ce1499a8e48d8c7e7c3ff94
Vinicius Perrone <perronevinicius2018@gmail.com>
2022-07-10 22:21:21 -0300

    feat: Rate Table CRUD

--- backend/credfica/admin.py
+++ backend/credfica/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from credfica.models import RateTable
+
 # Register your models here.
+
+admin.site.register(RateTable)
--- backend/credfica/models.py
+++ backend/credfica/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from uuid import uuid4
 
 # Create your models here.
+
+class RateTable(models.Model):
+  id = models.UUIDField(primary_key=True, default=uuid4)
+  name = models.CharField(max_length=255)
+  created_at = models.DateField(auto_now_add=True)
+
+  def __str__(self):
+    return self.name
