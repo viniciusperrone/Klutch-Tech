fd61e6090b3051b655feb9f212bfd3b01c2070e3
Vinicius Perrone <perronevinicius2018@gmail.com>
2022-07-13 12:49:30 -0300

    fix: url production

--- frontend/src/server/api.ts
+++ frontend/src/server/api.ts
@@ -1,7 +1,7 @@
-import axios from 'axios';
+import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.NEXT_API_URL || 'http://localhost:8000'
+  baseURL: process.env.NEXT_API_URL,
 });
 
 export default api;
